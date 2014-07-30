8284cbfb99044e5519d2f9779eb172b8e450439d
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2014-07-30 14:04:44 +0200

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+HCI Event Demo
+--
+
+Initial WIP version using event handler to deliver HCI EVT_TRANSACTION. Uses https://github.com/svic/mozilla-central/  Bug_979767 branch. 
---
+++ js/app.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+
+  function log(msg) {
+    console.log(msg);
+    var li = document.createElement('li');
+    li.textContent = new Date() + ' ' + msg;
+    document.getElementById('log').appendChild(li);
+  }
+
+  function byteArrayToHex(arr) {
+    if (!arr) {
+      return '';
+    }
+
+    var hexStr = '';
+    for (var i = 0; i < arr.length; i++) {
+      var hex = (arr[i] & 0xff).toString(16);
+      hex = (hex.length === 1) ? '0' + hex : hex;
+      hexStr += hex;
+    }
+    return hexStr;
+  }
+
+  function updateUIText(elementId, text) {
+    document.getElementById(elementId).textContent = text;
+  }
+
+  var firedCount = 0;
+  log('DOMContentLoaded, checking for NFC');
+  if (window.navigator.mozNfc) {
+    log('NFC found, registering event handler');
+    window.navigator.mozNfc.onhcieventtransaction = function(event) {
+      firedCount += 1;
+      log('HCI Event Transaction handler fired, count ' + firedCount);
+
+      updateUIText('count', firedCount);
+      updateUIText('aid', byteArrayToHex(event.detail.extra.aid));
+      updateUIText('data', byteArrayToHex(event.detail.extra.data));
+      updateUIText('uri', event.detail.uri);
+      updateUIText('time', new Date());
+    };
+  }
+});
